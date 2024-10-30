ddf5e7371a74c2354dac0bb778281cef05c30fa2
Shiva9361 <shivadharshansankar936@gmail.com>
2024-10-30 13:12:55 +0530

    fix: data?

--- scrape.py
+++ scrape.py
@@ -13,20 +13,8 @@ BASE_URL = "https://api.github.com"
 
 
 def create_user_csv():
-    search_url = f"""{
-        BASE_URL}/search/users?q=location:Tokyo&followers:>200&per_page=100"""
-    response = requests.get(search_url, auth=HTTPBasicAuth(
-        GITHUB_USERNAME, GITHUB_TOKEN))
-    # print(response.text)
-
-    if response.status_code != 200:
-        print(f"Error: {response.status_code}")
-
     required_data = ["login", "name", "company", "location", "email",
                      "hireable", "bio", "public_repos", "followers", "following", "created_at"]
-
-    data = response.json()
-    items = data.get('items', [])
 
     with open("user.csv", "w") as user_file:
 
@@ -34,32 +22,54 @@ def create_user_csv():
             user_file.write(f"{field},")
         user_file.write("\n")
 
-        for user in items:
-            # print(user)
-            user_data_url = f"{BASE_URL}/users/{user['login']}"
-            user_info = requests.get(user_data_url, auth=HTTPBasicAuth(
-                GITHUB_USERNAME, GITHUB_TOKEN)).json()
+    for page in range(1, 7):
+        search_url = f"""{
+            BASE_URL}/search/users?q=location:Tokyo&followers:>200&per_page=100&page={page}"""
+        response = requests.get(search_url, auth=HTTPBasicAuth(
+            GITHUB_USERNAME, GITHUB_TOKEN))
+        # print(response.text)
 
-            # print(user_info)
-        # users.append(user_info)
+        if response.status_code != 200:
+            print(f"Error: {response.status_code}")
+
+        required_data = ["login", "name", "company", "location", "email",
+                         "hireable", "bio", "public_repos", "followers", "following", "created_at"]
+
+        data = response.json()
+        items = data.get('items', [])
+
+        with open("user.csv", "a") as user_file:
+
             for field in required_data:
-                try:
-                    if (field == "company"):
-                        value = str(user_info[field]).strip(
-                            ' \t\n\r\n').strip().upper().lstrip("@")
-                        user_file.write(
-                            f'"{value}",')
-                    elif user_info[field] == None:
-                        user_file.write(",")
-                    elif user_info[field] == True:
-                        user_file.write("true,")
-                    elif user_info[field] == False:
-                        user_file.write("false,")
-                    else:
-                        user_file.write(f'"{user_info[field]}",')
-                except:
-                    user_file.write(f",")
+                user_file.write(f"{field},")
             user_file.write("\n")
+
+            for user in items:
+                # print(user)
+                user_data_url = f"{BASE_URL}/users/{user['login']}"
+                user_info = requests.get(user_data_url, auth=HTTPBasicAuth(
+                    GITHUB_USERNAME, GITHUB_TOKEN)).json()
+
+                # print(user_info)
+            # users.append(user_info)
+                for field in required_data:
+                    try:
+                        if (field == "company"):
+                            value = str(user_info[field]).strip(
+                                ' \t\n\r\n').strip().upper().lstrip("@")
+                            user_file.write(
+                                f'"{value}",')
+                        elif user_info[field] == None:
+                            user_file.write(",")
+                        elif user_info[field] == True:
+                            user_file.write("true,")
+                        elif user_info[field] == False:
+                            user_file.write("false,")
+                        else:
+                            user_file.write(f'"{user_info[field]}",')
+                    except:
+                        user_file.write(f",")
+                user_file.write("\n")
 
 
 def create_repo_csv():
